c594052ff1649077c2177d5b48a3799d761016fd
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-28 02:34:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,25 +2,6 @@ require 'pry'
 require 'yaml'
 
 def load_library(file)
-  new_hash = {
-   "get_meaning" => {},
-   "get_emoticon" => {}
- }
- library.each do |meaning, translation|
-   english = translation[0]
-   japanese = translation[1]
-   new_hash["get_meaning"][japanese] = meaning
-   new_hash["get_emoticon"][english] = japanese
- end
-  return new_hash
+  
 end
 
-
-def get_japanese_emoticon(file_path, emoticon)
-  library = load_library(file_path)
-  library["get_emoticon"].each do |key, value|
-    if key == emoticon
-      puts value
-    end
-  end
-end
