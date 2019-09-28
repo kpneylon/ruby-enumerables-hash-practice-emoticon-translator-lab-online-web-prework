62858a871a9c235213b7e5284600d63d0e48f13b
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-28 01:38:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,11 @@
-# require modules here
+require 'yaml'
+require 'pry'
 
-def load_library
-  # code goes here
-end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
+def load_library(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
+end	
