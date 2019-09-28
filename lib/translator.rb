24dfdfba343b0cf39ba6616eea9e89f004fe61b5
Kenneth Neylon <kenneth_neylon@hotmail.com>
2019-09-28 02:47:45 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,3 +12,11 @@ def load_library(file_path)
   result
 end
 
+def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
+  library = load_library(file_path)
+  if library["get_emoticon"].include?(english_emoticon) 
+    library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
+  end
+end
